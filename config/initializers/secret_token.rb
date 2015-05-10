0015ac010c02ab09f5202fda1524d60642fb8629
Allen Coin <allencoin@gmail.com>
2015-05-10 11:03:04 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Robutt::Application.config.secret_key_base = '8aa027dcf84d5cdda2524ba795a269e2a31832870d12a4aca1433aba77cb8274af579016f8a535203d289d3141ccc353586f0cb3241f1c50db0aec5f6394bde7'
